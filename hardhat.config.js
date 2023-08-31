92ddd1ffa7184116d8bfb8c72bfb3542e579ff39
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-08-31 08:26:29 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,110 +5,70 @@
 // - COMPILE_MODE:      production modes enables optimizations (default: development)
 // - COMPILE_VERSION:   compiler version (default: 0.8.20)
 // - COINMARKETCAP:     coinmarkercat api key for USD value in gas report
-
 const fs = require('fs');
 const path = require('path');
-const argv = require('yargs/yargs')()
-  .env('')
-  .options({
-    coverage: {
+const argv = require('yargs/yargs')().env('').options({
+    coverage: { type: 'boolean',
+      default: false, },gas: {alias: 'enableGasReport',
       type: 'boolean',
-      default: false,
-    },
-    gas: {
-      alias: 'enableGasReport',
-      type: 'boolean',
-      default: false,
-    },
-    gasReport: {
+      default: false,},gasReport: {
       alias: 'enableGasReportPath',
       type: 'string',
       implies: 'gas',
-      default: undefined,
-    },
-    mode: {
+      default: undefined, }, mode: {
       alias: 'compileMode',
       type: 'string',
       choices: ['production', 'development'],
-      default: 'development',
-    },
-    ir: {
+      default: 'development',}, ir: {
       alias: 'enableIR',
       type: 'boolean',
       default: false,
-    },
-    compiler: {
+    },compiler: {
       alias: 'compileVersion',
       type: 'string',
-      default: '0.8.20',
-    },
+      default: '0.8.20'},
     coinmarketcap: {
       alias: 'coinmarketcapApiKey',
-      type: 'string',
-    },
+      type: 'string',},
   }).argv;
-
-require('@nomiclabs/hardhat-truffle5');
-require('hardhat-ignore-warnings');
-require('hardhat-exposed');
-
-require('solidity-docgen');
-
+require('@nomiclabs/hardhat-truffle5');('hardhat-ignore-warnings');('hardhat-exposed');('solidity-docgen');
 for (const f of fs.readdirSync(path.join(__dirname, 'hardhat'))) {
   require(path.join(__dirname, 'hardhat', f));
 }
-
 const withOptimizations = argv.gas || argv.compileMode === 'production';
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+ */module.exports = {
   solidity: {
     version: argv.compiler,
     settings: {
       optimizer: {
         enabled: withOptimizations,
         runs: 200,
-      },
-      viaIR: withOptimizations && argv.ir,
+      },  viaIR: withOptimizations && argv.ir,
       outputSelection: { '*': { '*': ['storageLayout'] } },
-    },
-  },
-  warnings: {
+    },},warnings: {
     'contracts-exposed/**/*': {
       'code-size': 'off',
-      'initcode-size': 'off',
-    },
-    '*': {
-      'code-size': withOptimizations,
+      'initcode-size': 'off',},  '*': {'code-size': withOptimizations,
       'unused-param': !argv.coverage, // coverage causes unused-param warnings
-      default: 'error',
-    },
-  },
-  networks: {
+      default: 'error',},
+  }, networks: {
     hardhat: {
       blockGasLimit: 10000000,
-      allowUnlimitedContractSize: !withOptimizations,
-    },
-  },
-  exposed: {
-    initializers: true,
+      allowUnlimitedContractSize: !withOptimizations,},
+  },exposed: {initializers: true,
     exclude: ['vendor/**/*'],
-  },
-  docgen: require('./docs/config'),
+  }, docgen: require('./docs/config'),
 };
-
 if (argv.gas) {
   require('hardhat-gas-reporter');
   module.exports.gasReporter = {
     showMethodSig: true,
     currency: 'USD',
     outputFile: argv.gasReport,
-    coinmarketcap: argv.coinmarketcap,
-  };
+    coinmarketcap: argv.coinmarketcap,};
 }
-
 if (argv.coverage) {
   require('solidity-coverage');
   module.exports.networks.hardhat.initialBaseFeePerGas = 0;
