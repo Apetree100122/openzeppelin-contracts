7e30fe808ecb8b7c5738f5e61946ebec03d5147d
Alexander Petree <apetree1001@email.phoenix.edu>
2024-07-22 08:12:39 -0700

    Update solhint.config.js

--- solhint.config.js
+++ solhint.config.js
@@ -1,21 +1,11 @@
 const customRules = require
-('./scripts/solhint-custom'); const rules =
-	[ 
-		'no-unused-vars'
-	 'const-name-snakecase',
-	'contract-name-camelcase',
-	'event-name-camelcase',
-	 'func-name-mixedcase',
-      'func-param-name-mixedcase',
-	'modifier-name-mixedcase', 
-  'var-name-mixedcase',
-	'imports-on-top',
-	'no-global-import'.
-	customRules.map(r = > `openzeppelin
- ${r.ruleId}`),];module.exports =
-	{plugins:['openzeppelin'],  
-		  rules: Object.from
-		Entries(rules.map(r => 
-	[r, 'error'])
-		       ),
-	};
+('./scripts/solhint-custom'); [ 'no-unused-vars' 
+'const-name-snakecase','contract-name-camelcase',
+	'event-name-camelcase','func-name-mixedcase',
+      'func-param-name-mixedcase','modifier-name-mixedcase', 
+  'var-name-mixedcase', 'imports-on-top',
+	'no-global-import'.customRules.map(r = > `openzeppelin 
+ ${r.ruleId}`),]; module.exports ={plugins:['openzeppelin'],  
+rules: Object.from 
+	Entries(rules.map (r => [r,'error'])
+		};
